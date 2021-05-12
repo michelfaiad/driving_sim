54b02642c4fe283843aee5ee0c3910dae63a73ef
michelfaiad <michelfaiad@gmail.com>
2021-05-12 19:54:17 -0300

    cleanup

---
+++ Prototype 1/Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    [Header("Object References")]
+    [Tooltip("Player Object")]
+    [SerializeField] GameObject player;
+
+    //Private variables
+    Vector3 offset = new Vector3(0f, 6.87f, -9.03f);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player.transform.position + offset;
+    }
+}
---
+++ Prototype 1/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("Movement Configuration")]
+    [Tooltip("Horsepower")]
+    [SerializeField] float horsePower = 20000f;
+    [Tooltip("Turn Speed")]
+    [SerializeField] float turnSpeed = 25f;
+    [Tooltip("Max Speed")]
+    [SerializeField] int maxSpeed = 180;
+    [Header("Object References")]
+    [SerializeField] GameObject centerOfMass;
+    [SerializeField] TextMeshProUGUI speedText;
+    [SerializeField] TextMeshProUGUI rpmText;
+
+    //Private variables
+    float horizontalInput, forwardInput;
+    int speed;
+    int rpm;
+    Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.centerOfMass = centerOfMass.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        speed = Mathf.RoundToInt(rb.velocity.magnitude * 3.6f);
+        speedText.text = "Speed: " + speed + " Km/h";
+        rpm = Mathf.RoundToInt((speed % 30) * 40);
+        rpmText.text = "" + rpm;
+
+        //Getting player inputs
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+        //Moving forward
+        //transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        if (speed < maxSpeed)
+        {
+            rb.AddRelativeForce(Vector3.forward * horsePower * forwardInput);
+        }
+        //Rotates the car
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
+
+        
+    }
+}
